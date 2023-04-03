1860fb15f82f72a954e9ca41c686909bd362e96b
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-04-03 22:04:07 +0930

    Create enum to store error types and messages

---
+++ ErrorType.java
@@ -0,0 +1,12 @@
+public enum ErrorType {
+    OUT_OF_BOUNDS("Error! You must enter a row between A and J, and" +
+        " a column between 1 and 10 for both coordinates.\n"),
+    WRONG_LOCATION("Error! Wrong ship location.\n"),
+    WRONG_LENGTH("Error! Wrong length! The distance from head to tail must equal size of ship.\n"),
+    TOO_CLOSE("Error! You placed it too close to another one. Please try again.\n");
+
+    final String message;
+    ErrorType(String message) {
+        this.message = message;
+    }
+}
