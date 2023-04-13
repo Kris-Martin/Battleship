f0632e3236d78eedab1c33482000984defdcaa1c
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-04-14 00:06:06 +0930

    Move getFacing method to Compass

--- Compass.java
+++ Compass.java
@@ -1,3 +1,15 @@
 public enum Compass {
     NORTH, EAST, SOUTH, WEST;
+
+    public static Compass getFacing(Point headPos, Point tailPos) {
+        if (headPos.getX() == tailPos.getX() && headPos.getY() < tailPos.getY()) {
+            return Compass.WEST;
+        } else if (headPos.getX() == tailPos.getX() && headPos.getY() > tailPos.getY()) {
+            return Compass.EAST;
+        } else if (headPos.getY() == tailPos.getY() && headPos.getX() < tailPos.getX()) {
+            return Compass.NORTH;
+        } else {
+            return Compass.SOUTH;
+        }
+    }
 }
