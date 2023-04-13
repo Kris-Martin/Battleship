4dd83654ea6f751ff5d240d4a9a3305b3ac73b64
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-04-14 00:05:26 +0930

    Shorten file name

--- Cell.java
+++ Cell.java
@@ -1,11 +1,11 @@
-public enum CellType {
+public enum Cell {
     OCEAN('~'),
     SHIP('O'),
     HIT('X'),
     MISS('M');
     public final char value;
 
-    CellType(char value){
+    Cell(char value){
         this.value = value;
     }
 }
